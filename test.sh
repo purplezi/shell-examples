55ac9afc4d70f15cd1604bf0c74bb231745c337e
purplezi <654762676@qq.com>
2020-05-29 17:01:16 +0800

    test shellcheck

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ for file in ./*.sh;do
   if [[ $file =~ $0 ]]; then
     continue
   fi
-  printf "================ %s ================== \n" "$file"
+  printf "================== %s ================== \n" "$file"
   bash "$file"
+  printf "------------------ %s ------------------ \n" "$file"
+  shellcheck "$file"
 done
